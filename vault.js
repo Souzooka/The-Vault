6d0432da12af52181abc7552576785fab8e6f0bc
Dakota <bazookaharwell@gmail.com>
2017-03-14 18:26:13 -1000

    first 2 tests

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secret = {};
+
+  function getValue(key) {
+    if (secret[key] === undefined) {
+      return null;
+    }
+    return secret[key];
+  }
+
+  function setValue(key, newSecret) {
+    key = newKey;
+  }
+
+  return {
+    getValue,
+    setValue
+  }
+
 };
